5c62eba191f1eee73043edeea3ab280a8dbaf37a
Michael Stebel <michael@michaelstebelconsulting.com>
2024-12-12 11:55:19 -0500

    Version 7.18.6

---
+++ ISS Watch Extension/View Models/PassesViewModel.swift
@@ -0,0 +1,87 @@
+//
+//  PassesViewModel.swift
+//  ISS Watch
+//
+//  Created by Michael Stebel on 6/5/24.
+//  Copyright © 2024-2025 ISS Real-Time Tracker. All rights reserved.
+//
+
+import CoreLocation
+import SwiftUI
+
+@Observable
+final class PassesViewModel: ObservableObject {
+    
+    // MARK: Published properties
+    
+    var isComputing     = false
+    var noradID: String = ""
+    var predictedPasses = [Passes.Pass]()
+    
+    private var lat: Double = 5
+    private var lon: Double = 5
+    
+    /// Get array of predicted passes
+    /// - Parameters:
+    ///   - noradCode: Norad code for satellite
+    ///   - latitude: User's latitude
+    ///   - longitude: User's longitude
+    func getPasses(for noradCode: String, latitude: Double, longitude: Double) {
+        Task {
+            isComputing.toggle()
+            lat = latitude
+            lon = longitude
+            noradID = noradCode
+            if let passes = await fetchData() {
+                predictedPasses = passes.passes
+                isComputing.toggle()
+            }
+        }
+    }
+    
+    /// Get the number of stars from the rating system enum
+    /// If no magnitude was returned by the API, return nil
+    /// - Parameters:
+    ///   - magnitude: pass magnitude
+    /// - Returns: optional Int
+    func getNumberOfStars(forMagnitude magnitude: Double) -> Int? {        
+        if magnitude != RatingSystem.unknown.rawValue {
+            return RatingSystem.numberOfRatingStars(for: magnitude)
+        } else {
+            return nil
+        }
+    }
+    
+    /// Get the next pass
+    /// - Returns: An arrray of Passes
+    private func fetchData() async -> Passes? {
+        let altitude             = 0
+        let apiKey               = ApiKeys.passesApiKey
+        let endpointForPassesAPI = ApiEndpoints.passesAPIEndpoint
+        let minObservationTime   = 300
+        let numberOfDays         = 30
+        let stationID            = noradID
+
+        // Create the API URL request from endpoint. If not succesful, then return
+        let URLrequestString = endpointForPassesAPI + "\(stationID)/\(lat)/\(lon)/\(altitude)/\(numberOfDays)/\(minObservationTime)&apiKey=\(apiKey)"
+        
+        // Ensure the URL is valid
+        guard let url = URL(string: URLrequestString) else {
+            print("Invalid URL")
+            return nil
+        }
+        do {
+            // Fetch the data asynchronously
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            // Decode the data into the Passes object
+            let decoder = JSONDecoder()
+            let apiData = try decoder.decode(Passes.self, from: data)
+            return apiData
+        } catch {
+            // Handle errors
+            print("Error getting passes: \(error.localizedDescription)")
+            return nil
+        }
+    }
+}
---
+++ ISS Watch Extension/Views/Globe/GlobeView.swift
@@ -0,0 +1,120 @@
+//
+//  GlobeView.swift
+//  ISS Watch
+//
+//  Created by Michael Stebel on 8/26/21.
+//  Copyright © 2021-2025 ISS Real-Time Tracker. All rights reserved.
+//
+
+import SwiftUI
+import SceneKit
+
+struct GlobeView: View {
+    
+    // Get the current phase of the scene
+    @Environment(\.scenePhase) private var scenePhase
+    
+    // We're observing our view model
+    @StateObject private var vm = PositionViewModel()
+    
+    var body: some View {
+        
+        NavigationStack {
+            
+            ZStack {
+                
+                // MARK: Globe
+                
+                // Render the globe. This will update continually as new coordinates are received from the view model.
+                SceneView(scene: vm.globeScene,
+                          pointOfView: vm.globeMainNode,
+                          options: [.allowsCameraControl]
+                )
+                
+                // Show progress indicator when starting up/resetting
+                if vm.isStartingUp {
+                    ProgressView()
+                        .scaleEffect(x: 2, y: 2, anchor: .center) // Scale the ProgressView
+                }
+                
+                // MARK: Buttons
+                
+                VStack {
+                    Spacer() // we want the buttons at the bottom of the screen
+                    
+                    // Button group
+                    HStack(spacing: -35) {
+                        Button(action: {
+                            vm.reset()                              // Reset globe
+                        })
+                        {
+                            Image(systemName: vm.isStartingUp ? "arrow.triangle.2.circlepath" : "arrow.circlepath")
+                                .contentTransition(.symbolEffect(.replace.wholeSymbol))
+                        }
+                        .withSmallButtonModifier
+                        
+                        Button(action: {
+                            vm.spinEnabled.toggle()                 // Toggle globe rotation
+                        })
+                        {
+                            Image(systemName: vm.spinEnabled ? "rotate.3d.fill" : "rotate.3d")
+                                .contentTransition(.symbolEffect(.replace.wholeSymbol))
+                        }
+                        .withSmallButtonModifier
+                        
+                        NavigationLink(
+                            destination: DetailView(vm: vm)         // Show detail view with data from our view model
+                        )
+                        {
+                            Image(systemName: "tablecells")
+                                .scaleEffect(0.9)
+                        }
+                        .withSmallButtonModifier
+                        
+                        NavigationLink(
+                            destination: CrewView()                 // Show crew view
+                        )
+                        {
+                            Image(systemName: "person.fill")
+                        }
+                        .withSmallButtonModifier
+                    }
+                    .padding([.horizontal])
+                    .padding([.bottom], 3.0)
+                }
+                // Pop up an alert if there was an error fetching data
+                .alert(isPresented: $vm.wasError) {
+                    Alert(title: Text(vm.errorForAlert?.title ?? "Oops!"),
+                          message: Text(vm.errorForAlert?.message ?? "Can't get data.")
+                    )
+                }
+            }
+            .ignoresSafeArea(edges: [.vertical])
+            .navigationTitle("Globe")
+            .navigationBarTitleDisplayMode(.inline)     // We want the small navigation title
+            
+            // MARK: Handle lifecycle events
+            
+            .onChange(of: scenePhase) { _, phase in
+                switch phase {
+                case .active:
+                    // The scene has become active, so start updating
+                    vm.start()
+                case .inactive, .background:
+                    // The app has become inactive, so stop updating
+                    vm.stop()
+                @unknown default:
+                    fatalError("The app has entered an unknown state.")
+                }
+            }
+        }
+    }
+}
+    
+struct GlobeView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            GlobeView()
+        }
+    }
+}
